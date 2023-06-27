de49bf97de191be59110781de1d46f0952adf6dd
Meera-Mohanan <126405858+Meera-Mohanan@users.noreply.github.com>
2023-06-27 23:30:33 +0800

    seeder added

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,76 +1,25 @@
-const axios = require('axios');
 const sequelize = require('./sequelize');
-const User = require('./models/user');
-const Review = require('./models/review');
 const Book = require('./models/book');
-
-async function seedUsers() {
-  try {
-    // Read the userSeeder.json file
-    const userSeedData = require('./seeders/userSeeder.json');
-
-    // Perform the seeding logic for user data
-    await User.bulkCreate(userSeedData.users);
-
-    console.log('User seeding completed!');
-  } catch (error) {
-    console.error('Error seeding users:', error);
-  }
-}
-
-async function seedReviews() {
-  try {
-    // Read the reviewSeeder.json file
-    const reviewSeedData = require('./seeders/reviewSeeder.json');
-
-    // Perform the seeding logic for review data
-    await Review.bulkCreate(reviewSeedData.reviews);
-
-    console.log('Review seeding completed!');
-  } catch (error) {
-    console.error('Error seeding reviews:', error);
-  }
-}
-
-async function seedBooks() {
-  try {
-    // Fetch book data from the Google Books API
-    const response = await axios.get('https://www.googleapis.com/books/v1/volumes?q=javascript');
-    const apiBooks = response.data.items;
-
-    // Extract relevant data from API response
-    const bookData = apiBooks.map(book => {
-      return {
-        title: book.volumeInfo.title,
-        author: book.volumeInfo.authors && book.volumeInfo.authors.join(', '),
-        genre: book.volumeInfo.categories && book.volumeInfo.categories.join(', '),
-        publishedDate: book.volumeInfo.publishedDate
-      };
-    });
-
-    // Perform the seeding logic for book data
-    await Book.bulkCreate(bookData);
-
-    console.log('Book seeding completed!');
-  } catch (error) {
-    console.error('Error seeding books:', error);
-  }
-}
+const Review = require('./models/review');
+const bookSeedData = require('./bookSeeder.json');
+const reviewSeedData = require('./reviewSeeder.json');
 
 async function seedDatabase() {
   try {
-    // Seed the data
-    await seedUsers();
-    await seedReviews();
-    await seedBooks();
+    // Seed books
+    await Book.bulkCreate(bookSeedData.books);
+    console.log('Books seeded successfully.');
+
+    // Seed reviews
+    await Review.bulkCreate(reviewSeedData.reviews);
+    console.log('Reviews seeded successfully.');
 
     console.log('Database seeding completed!');
   } catch (error) {
     console.error('Error seeding database:', error);
   } finally {
-    sequelize.close(); // Close the database connection after seeding
+    sequelize.close(); // Close the database connection
   }
 }
 
-// Invoke the seedDatabase function
 seedDatabase();
