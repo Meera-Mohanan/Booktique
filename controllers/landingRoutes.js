9ad8818ce15b116e4a948370235da4ec1a2ac790
vijaycheruku <126775249+vijaycheruku@users.noreply.github.com>
2023-06-27 15:26:06 +0800

    display handlebars added

--- controllers/landingRoutes.js
+++ controllers/landingRoutes.js
@@ -3,7 +3,8 @@ const router = require('express').Router();
 
 router.get('/', (req,res) => {
 
-    res.json({ data: 'hi' })
+    // res.json({ data: 'hi' })
+    res.render('landingpage');
 
 });
 
--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 const express = require('express');
-const path = require('path');
-const sequelize = require('./config/connection');
 const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const path = require('path');
+
+const helpers = require('./utils/helper');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+const session = require('express-session');
+
+const PORT = process.env.PORT || 3001;
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const ses = {
+    secret: 'secret',
+    cookie: { maxAge: 36000 },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 const app = express();
-const PORT = process.env.port || 3001;
-
+app.use(session(ses));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(routes);
 
