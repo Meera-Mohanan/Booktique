e6717d22f4f49c1f963f7cbd6398b25803de7618
vijaycheruku <126775249+vijaycheruku@users.noreply.github.com>
2023-06-27 14:07:03 +0800

    project structure created

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS booktique;
+
+CREATE DATABASE booktique;
---
+++ utils/auth.js
@@ -0,0 +1,9 @@
+const auth = (req, res, next) => {
+    if(!req.session.userid) {
+        res.redirect('/login');
+    }else{
+        next();
+    }
+}
+
+module.exports = auth;
