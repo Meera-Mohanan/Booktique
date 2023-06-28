d965627bad5f47a4bf81279d31b3eb2b8a25046c
anju <anjualfino@gmail.com>
2023-06-28 23:30:16 +0800

    adding books routes

--- routes/api/books-routes.js
+++ routes/api/books-routes.js
@@ -1,64 +1,136 @@
 const router = require('express').Router();
 const axios = require('axios');
+const { Book } = require('../models');
+
+//list of routers--
+// Route to add a book to the books table from the Google Books API (parameter-bookid)
+// Route to get all books from the database
+// Route to search for a book by bookid in the database
+// Route for searching books by type from api
+// Route for searching books by author or book name from api
+
+//implement later--get a bookid from google api when clicking on a book
+
+// Route to add a book to the books table from the Google Books API
+router.post('/add/:bookId', async (req, res) => {
+    try {
+      const { bookId } = req.params;
+  
+      // Fetch book details from the Google Books API using the bookId
+      const apiUrl = `https://www.googleapis.com/books/v1/volumes/${bookId}`;
+      const response = await axios.get(apiUrl);
+      const bookData = response.data;
+  
+      // Extract the relevant book information
+      const { title, authors, categories, publishedDate, description } = bookData.volumeInfo;
+  
+      // Create a new book instance in the database
+      const newBook = await Book.create({
+        bookId,
+        title,
+        author: authors.join(', '),
+        genre: categories.join(', '),
+        publicationDate: publishedDate,
+        description,
+      });
+  
+      res.status(201).json(newBook);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
 
 
-//get books from database
+// Route to get all books from the database
+router.get('/all', async (req, res) => {
+  try {
+    const books = await books.findAll(); // Retrieve all books from the books table
+    res.json(books); // Return the books as a JSON response
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Route to search for a book by bookid in the database
+router.get('/:bookid', async (req, res) => {
+    try {
+      const { bookid } = req.params;
+  
+      // Search for the book in the database by bookid
+      const book = await Book.findOne({ bookid });
+  
+      if (!book) {
+        return res.status(404).json({ error: 'Book not found' });
+      }
+  
+      res.json(book);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+  
 
 
-// Route for searching books by type
+// Route for searching books by type from api
 router.get('/search/:type', async (req, res) => {
     try {
-      const apiKey = process.env.GOOGLE_BOOKS_API_KEY;
-      const searchType = req.params.type; // Get the search type from the request URL parameter
-  
-      const url = `https://www.googleapis.com/books/v1/volumes`;
-  
-      const params = {
-        q: `type:${searchType}`, // Add the search type parameter to the query
-        key: apiKey
-      };
-  
-      const response = await axios.get(url, { params });
-      console.log();
-      const books = response.data.items; 
-  
-      // Process the books as needed
-      res.json(books); // Return the books as a JSON response
-  
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
+        const apiKey = process.env.GOOGLE_BOOKS_API_KEY;
+        const searchType = req.params.type; // Get the search type from the request URL parameter
 
-  // Route for searching books by author or book name
-router.get('/books/:query', async (req, res) => {
-    try {
-      const apiKey = process.env.GOOGLE_BOOKS_API_KEY;
-      const searchQuery = req.params.query; // Get the search query from the request URL parameter
-  
-      const url = 'https://www.googleapis.com/books/v1/volumes';
-  
-      const params = {
-        q: `inauthor:${searchQuery}+OR+intitle:${searchQuery}`, // Search by author or book name
-        key: apiKey
-      };
-  
-      const response = await axios.get(url, { params });
-  
-      const books = response.data.items; // Extract the books from the API response
-  
-      // Process the books as needed
-      res.json(books); // Return the books as a JSON response
-  
+        const url = `https://www.googleapis.com/books/v1/volumes`;
+
+        const params = {
+            q: `type:${searchType}`, // Add the search type parameter to the query
+            key: apiKey
+        };
+
+        const response = await axios.get(url, { params });
+        console.log();
+        const books = response.data.items;
+
+        // Process the books as needed
+        res.json(books); // Return the books as a JSON response
+
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
-  });
-  
- 
-/* // Get books from Google Books API based on a book  title
+});
+
+// Route for searching books by author or book name from api
+router.get('/search/:query', async (req, res) => {
+    try {
+        const apiKey = process.env.GOOGLE_BOOKS_API_KEY;
+        const searchQuery = req.params.query; // Get the search query from the request URL parameter
+
+        const url = 'https://www.googleapis.com/books/v1/volumes';
+
+        const params = {
+            q: `inauthor:${searchQuery}+OR+intitle:${searchQuery}`, // Search by author or book name
+            key: apiKey
+        };
+
+        const response = await axios.get(url, { params });
+
+        const books = response.data.items; // Extract the books from the API response
+
+        // Process the books as needed
+        res.json(books); // Return the books as a JSON response
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+module.exports = router;
+
+
+
+/* // Get books from Google Books API based on a book  title from api
 router.get('/:name', async (req, res) => {
     try {
         const apiKey = process.env.GOOGLE_BOOKS_API_KEY;
@@ -83,7 +155,7 @@ router.get('/:name', async (req, res) => {
 });
 
 
-  // Route for searching books by author
+  // Route for searching books by author from api
 router.get('/books/author/:authorName', async (req, res) => {
     try {
       const apiKey = process.env.GOOGLE_BOOKS_API_KEY;
@@ -106,8 +178,7 @@ router.get('/books/author/:authorName', async (req, res) => {
       res.status(500).json({ error: 'Internal Server Error' });
     }
   }); */
-  
-  
-  
 
-module.exports = router;
+
+
+
