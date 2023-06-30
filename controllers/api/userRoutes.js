349201917cafb37b9e62643bd86394fd1dee7e40
Sabin budhathoki <budhathokisabin303@gmail.com>
2023-06-30 18:11:48 +0800

    autentication update

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+// Login route
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
@@ -33,10 +34,32 @@ router.post('/login', async (req, res) => {
   }
 });
 
-router.post('/logout', (req, res) => {
+// Signup route
+router.post('/signup', async (req, res) => {
+  try {
+    // Extract the required fields from req.body, such as username, password, etc.
+    const { username, password, email } = req.body;
+
+    // Implement logic to create a new user in your database
+    const newUser = await User.create({ username, password, email });
+
+    // Save the new user's information to the session and set logged_in to true
+    req.session.save(() => {
+      req.session.user_id = newUser.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: newUser, message: 'You are now registered and logged in!' });
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Logout route
+router.get('/logout', (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
-      res.status(204).end();
+      res.redirect('/login'); // Redirect to the login page
     });
   } else {
     res.status(404).end();
--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require('express');
 const path = require('path');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
-
 const helpers = require('./utils/helper');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ helpers });
 const session = require('express-session');
-
-const PORT = process.env.PORT || 3001;
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
+
 const ses = {
     secret: 'secret',
     cookie: { maxAge: 36000 },
@@ -20,7 +22,7 @@ const ses = {
     })
 };
 
-const app = express();
+
 app.use(session(ses));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
