d0c3eff6e762c2c8a79fa66d5424a5ffed131d3e
Sabin budhathoki <budhathokisabin303@gmail.com>
2023-07-01 15:13:32 +0800

    updated authentication

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3306;
 
 const hbs = exphbs.create({ helpers });
 
@@ -33,6 +33,54 @@ app.set('view engine', 'handlebars');
 
 app.use(routes); 
 
+// User registration
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    // Check if the user already exists
+    const existingUser = await User.findOne({ where: { username } });
+    if (existingUser) {
+      return res.status(400).json({ message: 'Username already taken' });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create the user
+    const user = await User.create({ username, password: hashedPassword });
+
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
+// User login
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    // Check if the user exists
+    const user = await User.findOne({ where: { username } });
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid username or password' });
+    }
+
+    // Check if the password is correct
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid username or password' });
+    }
+
+    // Generate a JWT token
+    const token = jwt.sign({ userId: user.id }, 'your-secret-key');
+
+    res.status(200).json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
 
 sequelize
   .sync({ force: false })
