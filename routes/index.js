61ab42cc95d186ce7caa3e43b367b6fcbf129cb9
vijaycheruku <126775249+vijaycheruku@users.noreply.github.com>
2023-07-03 11:44:08 +0800

    landingpageroutes added

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,14 @@ require('dotenv').config();
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
+const landingRoutes = require('./landingRoutes.js');
+
+
+router.use('/',landingRoutes);
 router.use('/api', apiRoutes);
 
-router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
-});
+// router.use((req, res) => {
+//   res.send("<h1>Wrong Route!</h1>")
+// });
 
 module.exports = router;
---
+++ routes/landingRoutes.js
@@ -0,0 +1,50 @@
+const router = require('express').Router();
+
+
+router.get('/', (req,res) => {
+
+    // res.json({ data: 'hi' })
+    res.render('landingpage', {loggedIn: true});
+    // res.render('landingpage', { post, loggedIn: true });
+
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+router.get('/review', (req, res) => {
+    res.render('yourreviews', {loggedIn: true});
+})
+
+
+
+router.get('/profilesettings', (req, res) => {
+    res.render('profilesettings', {loggedIn: true});
+})
+
+router.get('/bookreview', (req, res) => {
+    res.render('bookreview', {loggedIn: true});
+})
+
+
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+module.exports = router;
