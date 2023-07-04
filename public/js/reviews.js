04408cc0ae9a4526527f334da8a28202cf9bb582
anju <anjualfino@gmail.com>
2023-07-04 20:45:11 +0800

    review changes

--- public/js/reviews.js
+++ public/js/reviews.js
@@ -3,9 +3,9 @@ const delButtonHandler = async (event) => {
   event.preventDefault();
 
   if (event.target.hasAttribute('data-id')) {
-    //const id = event.target.getAttribute('data-id');
+  const id = event.target.getAttribute('data-id');
 
-    const response = await fetch(`/api/review/${data-id}`, {
+    const response = await fetch(`/api/review/${id}`, {
       method: 'DELETE',
     });
 
