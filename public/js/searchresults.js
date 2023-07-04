cd0ba5c28a5391bab5fffd15690dd6ef70eb4a17
Meera-Mohanan <126405858+Meera-Mohanan@users.noreply.github.com>
2023-07-04 18:59:18 +0800

    merge main to meera

--- public/js/searchresults.js
+++ public/js/searchresults.js
@@ -1,23 +1,53 @@
-$(document).ready(function(){
-   
-
-    $("#myform").submit(function(){
-        const search=$("#books").val();
-        if(search=='')
-        {
-            alert("Please enter something in the field first");
-         }
-        
-    });
-
-    else{
-        const url='';
-        const img='';
-        const title='';
-        const author='';
-
-        $.get("https://www.googleapis.com/books/v1/volumes/?q="+ search,function(response){})
+async function newReviewHandler(event) {
+    event.preventDefault();
+  
+    const search = document.querySelector('#books').value.trim();
+    if (!search) {
+      alert('Please enter something in the field first');
+      return;
     }
-      
-});
-    return false;
+  
+    try {
+      const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${search}`);
+      if (response.ok) {
+        const data = await response.json();
+        const searchResults = data.items.slice(0, 5);
+  
+        const searchResultsContainer = document.querySelector('#searchResults');
+        searchResultsContainer.innerHTML = '';
+  
+        searchResults.forEach((item) => {
+          const { title, authors, imageLinks } = item.volumeInfo;
+          const bookElement = createBookElement(title, authors, imageLinks);
+          searchResultsContainer.appendChild(bookElement);
+        });
+      } else {
+        throw new Error('Request failed');
+      }
+    } catch (error) {
+      console.log('Error:', error.message);
+      alert('Failed to fetch search results');
+    }
+  }
+  
+  function createBookElement(title, authors, imageLinks) {
+    const bookElement = document.createElement('div');
+    bookElement.classList.add('search-item');
+  
+    const imageElement = document.createElement('img');
+    imageElement.src = imageLinks?.thumbnail || 'placeholder-image.jpg';
+    imageElement.alt = title;
+  
+    const titleElement = document.createElement('h3');
+    titleElement.textContent = title;
+  
+    const authorsElement = document.createElement('p');
+    authorsElement.textContent = authors ? authors.join(', ') : 'Unknown Author';
+  
+    bookElement.appendChild(imageElement);
+    bookElement.appendChild(titleElement);
+    bookElement.appendChild(authorsElement);
+  
+    return bookElement;
+  }
+  
