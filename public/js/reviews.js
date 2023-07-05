2f2ac0a826b5f223f9af5830778945ed12a1a5cd
Meera-Mohanan <126405858+Meera-Mohanan@users.noreply.github.com>
2023-07-05 18:52:52 +0800

    reviews.js updated to latest

--- public/js/reviews.js
+++ public/js/reviews.js
@@ -1,21 +1,83 @@
-
-const delButtonHandler = async (event) => {
+ /* const newFormHandler = async (event) => {
   event.preventDefault();
+ // Retrieve the logged-in user's ID from the session data
+      //const userId = req.user.id;
+      const userId=1;
 
-  if (event.target.hasAttribute('data-id')) {
-  const id = event.target.getAttribute('data-id');
-
-    const response = await fetch(`/api/review/${id}`, {
-      method: 'DELETE',
+  if (userId) {
+    const response = await fetch(`/review/user/${userId}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
     });
 
     if (response.ok) {
-      // Reload the page or update the UI as needed
-      location.reload('/reviews');
+      document.location.replace('/profile');
     } else {
-      alert('Failed to delete review');
+      alert('Failed to create project');
+    }
+  } 
+};*/
+
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const reviews = await fetch(`/api/projects/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (reviews.ok) {
+      //const reviews = await fetchUserReviews(userId);
+      res.render('yourreviews', { reviews });
+    } else {
+      alert('Failed to delete project');
     }
   }
 };
 
-document.querySelector('#delete-review').addEventListener('click', delButtonHandler);
+/* document
+  .querySelector('.new-project-form')
+  .addEventListener('submit', newFormHandler);
+
+document
+  .querySelector('.project-list')
+  .addEventListener('click', delButtonHandler);
+ */
+
+
+
+/* async function reviewsHandler(event) {
+
+    event.preventDefault();
+
+    // const id;
+
+
+
+
+
+
+async function reviewsHandler(req, res) {
+    try {
+      // Retrieve the logged-in user's ID from the session data
+      //const userId = req.user.id;
+     const userId=1;
+      // Perform any necessary data retrieval or API requests using the user's ID
+      const reviews = await fetchUserReviews(userId);
+        
+      res.render('yourreviews', { reviews });
+    } catch (error) {
+      // Handle any errors that occur during the data retrieval or rendering
+      console.error('Error retrieving or rendering reviews:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+
+  } 
+
+  
+
+
+  */
