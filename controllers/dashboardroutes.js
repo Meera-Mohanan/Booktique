5a50c79bdf6bbeb25a64d576304d73a7de864b85
Meera-Mohanan <126405858+Meera-Mohanan@users.noreply.github.com>
2023-07-05 19:01:17 +0800

    controllers deleted

--- controllers/dashboardroutes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection');
-const withAuth = require('../utils/auth');
-
-//Dashboard page routes
-router.get('/', withAuth, (req, res) => {
-    
-});
