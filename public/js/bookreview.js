203de27c748240645b69876b99da1403e092799d
Sabin budhathoki <budhathokisabin303@gmail.com>
2023-07-06 19:41:46 +0800

    updated review page with book details

--- public/js/bookreview.js
+++ public/js/bookreview.js
@@ -1,7 +1,41 @@
-// Function to handle the button click event
-function handleBookDetailsClick(bookId) {
-  // Redirect to the book details page with the book ID as a query parameter
-  window.location.href = `/bookdetails.handlebars?id=${bookId}`;
+// Function to create and show the book details page
+function showBookDetails(book) {
+  // Clear the existing content on the page
+  document.body.innerHTML = '';
+
+  // Create a big container for the book details
+  const bigContainer = document.createElement('div');
+  bigContainer.classList.add('big-container');
+
+  // Create elements to display the book details
+  const titleElement = document.createElement('h2');
+  titleElement.textContent = book.title;
+
+  const authorElement = document.createElement('p');
+  authorElement.textContent = `Author: ${book.author}`;
+
+  const published_dateElement = document.createElement('p');
+  published_dateElement.textContent = `Published Date: ${book.published_date}`;
+
+  const genreElement = document.createElement('p');
+  genreElement.textContent = `Genre: ${book.genre}`;
+
+  const descriptionElement = document.createElement('p');
+  descriptionElement.textContent = `Description: ${book.description}`;
+
+  const reviewsElement = document.createElement('p');
+  reviewsElement.textContent = `Reviews: ${book.reviews}`;
+
+  // Append book details to the big container
+  bigContainer.appendChild(titleElement);
+  bigContainer.appendChild(authorElement);
+  bigContainer.appendChild(published_dateElement);
+  bigContainer.appendChild(genreElement);
+  bigContainer.appendChild(descriptionElement);
+  bigContainer.appendChild(reviewsElement);
+
+  // Append the big container to the body
+  document.body.appendChild(bigContainer);
 }
 
 // Fetch books from your custom router and render the book list
@@ -19,10 +53,10 @@ async function fetchBooks() {
       const title = book.title;
       const author = book.author;
       const published_date = book.published_date;
-      const bookId = book.id;
+      const book_id = book.id;
       const reviews = book.reviews;
       const genre = book.genre;
-      const description =book.description;
+      const description = book.description;
 
       // Create a container for each book
       const bookContainer = document.createElement('div');
@@ -39,41 +73,39 @@ async function fetchBooks() {
       published_dateElement.textContent = `Published Date: ${published_date}`;
 
       const genreElement = document.createElement('p');
-      genreElement.textContent =`Genre: ${genre}`;
+      genreElement.textContent = `Genre: ${genre}`;
 
       const descriptionElement = document.createElement('p');
-      descriptionElement.textContent =`Description: ${description}`;
+      descriptionElement.textContent = `Description: ${description}`;
 
       const reviewsElement = document.createElement('p');
-      reviewsElement.textContent =`Reviews: ${reviews}`;
+      reviewsElement.textContent = `Reviews: ${reviews}`;
 
       // Create a button for book details
       const detailsButton = document.createElement('button');
       detailsButton.textContent = 'Book Details';
 
-      // Add click event listener to handle book details button click
+      // Add click event listener to show book details
       detailsButton.addEventListener('click', () => {
-        handleBookDetailsClick(bookId);
+        showBookDetails(book);
       });
-      
 
-      // Append book details to the book container
+      // Append book details and button to the book container
       bookContainer.appendChild(titleElement);
       bookContainer.appendChild(authorElement);
       bookContainer.appendChild(published_dateElement);
       bookContainer.appendChild(genreElement);
-      bookContainer.appendChild(reviewsElement);
       bookContainer.appendChild(descriptionElement);
+      bookContainer.appendChild(reviewsElement);
       bookContainer.appendChild(detailsButton);
 
       // Append the book container to the book list
       bookList.appendChild(bookContainer);
     }
-  } catch (error) {
+  }catch (error) {
     console.error(error);
   }
 }
-
 
 // Call the fetchBooks function to load and display the book data
 fetchBooks();
