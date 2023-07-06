49336728ccabeb6448d1345de67cefce398167fe
Meera-Mohanan <126405858+Meera-Mohanan@users.noreply.github.com>
2023-07-06 13:17:14 +0800

    search function updated

--- public/js/searchresults.js
+++ public/js/searchresults.js
@@ -27,12 +27,104 @@ function createBookElement(title, author, genre, user, image_url) {
   return bookElement;
 }
 
+// // Add an event listener to the search form
+// document.getElementById('searchForm').addEventListener('submit', function (event) {
+//   event.preventDefault(); // Prevent form submission
+
+//   // Get the search query from the input field
+//   const searchQuery = document.getElementById('searchbook').value.trim();
+
+//   if (!searchQuery) {
+//     alert('Please enter a search query');
+//     return;
+//   }
+
+//   performSearch(searchQuery); // Call the function to perform the search
+// });
+
+// // Function to perform the search
+// async function performSearch(query) {
+//   try {
+//     console.log('Performing search:', query); // Add console.log statement
+//     const response = await fetch('https://www.googleapis.com/books/v1/volumes?q=${query}');
+//     if (response.ok) {
+//       const searchResults = await response.json();
+//       displaySearchResults(searchResults);
+//     } else {
+//       throw new Error('Request failed');
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     alert('Failed to fetch search results');
+//   }
+// }
+
+// // Function to display the search results
+// function displaySearchResults(results) {
+//   const searchResultsContainer = document.getElementById('searchResultsContainer');
+//   searchResultsContainer.innerHTML = '';
+
+//   if (results.length > 0) {
+//     results.forEach((item) => {
+//       const { title, author, genre, user, image_url } = item;
+//       const bookElement = createBookElement(title, author, genre, user, image_url);
+//       searchResultsContainer.appendChild(bookElement);
+//     });
+//   } else {
+//     const noResultsElement = document.createElement('p');
+//     noResultsElement.textContent = 'No results found.';
+//     searchResultsContainer.appendChild(noResultsElement);
+//   }
+// }
+
+// Add an event listener to the search input field
+const searchInput = document.getElementById('searchbook');
+searchInput.addEventListener('input', function () {
+  const searchQuery = searchInput.value.trim();
+  if (searchQuery) {
+    performAutocomplete(searchQuery);
+  }
+});
+
+// Function to perform autocomplete
+async function performAutocomplete(query) {
+  try {
+    const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+    if (response.ok) {
+      const autocompleteResults = await response.json();
+      displayAutocompleteResults(autocompleteResults);
+    } else {
+      throw new Error('Request failed');
+    }
+  } catch (error) {
+    console.error(error);
+    alert('Failed to fetch autocomplete results');
+  }
+}
+
+// Function to display the autocomplete results
+function displayAutocompleteResults(results) {
+  const autocompleteResultsContainer = document.getElementById('autocompleteResultsContainer');
+  autocompleteResultsContainer.innerHTML = '';
+
+  if (results.length > 0) {
+    results.forEach((result) => {
+      const optionElement = document.createElement('option');
+      optionElement.value = result;
+      autocompleteResultsContainer.appendChild(optionElement);
+    });
+  }
+}
+
 // Add an event listener to the search form
 document.getElementById('searchForm').addEventListener('submit', function (event) {
   event.preventDefault(); // Prevent form submission
 
-  // Get the search query from the input field
-  const searchQuery = document.getElementById('searchbook').value.trim();
+  const selectedOption = document.getElementById('autocompleteResultsContainer').value;
+  let searchQuery = searchInput.value.trim();
+  if (selectedOption) {
+    searchQuery = selectedOption;
+  }
 
   if (!searchQuery) {
     alert('Please enter a search query');
@@ -45,8 +137,7 @@ document.getElementById('searchForm').addEventListener('submit', function (event
 // Function to perform the search
 async function performSearch(query) {
   try {
-    console.log('Performing search:', query); // Add console.log statement
-    const response = await fetch('https://www.googleapis.com/books/v1/volumes?q=${query}');
+    const response = await fetch(`/api/search?search=${query}`);
     if (response.ok) {
       const searchResults = await response.json();
       displaySearchResults(searchResults);
@@ -76,3 +167,4 @@ function displaySearchResults(results) {
     searchResultsContainer.appendChild(noResultsElement);
   }
 }
+
