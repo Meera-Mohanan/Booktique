8d1c7be9f75bb22f2184b9eb00ba9f2610d81217
Sabin budhathoki <budhathokisabin303@gmail.com>
2023-07-09 12:15:16 +0800

    updated working server

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,18 @@
 require('dotenv').config({
-    path:__dirname+'/../.env2'
-  });
-  
-  const Sequelize = require('sequelize');
-  
-  const sequelize = process.env.JAWSDB_URL
-    ? new Sequelize(process.env.JAWSDB_URL)
-    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-        host: 'localhost',
-        port: 3306,
-        dialect: 'mysql',
-        dialectOptions: {
-          decimalNumbers: true,
-        },
-      });
-  
-  module.exports = sequelize;
+  path:__dirname+'/../.env2'
+});
+
+const Sequelize = require('sequelize');
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: 'localhost',
+      port: 3306,
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
+
+module.exports = sequelize;
