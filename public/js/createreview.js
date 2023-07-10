0f163834f0e948d733e57fc34d7ca2c5331a3ea0
Meera-Mohanan <126405858+Meera-Mohanan@users.noreply.github.com>
2023-07-10 13:40:45 +0800

    unable to save reviews

--- public/js/createreview.js
+++ public/js/createreview.js
@@ -3,8 +3,12 @@ async function newReviewHandler(event) {
     event.preventDefault();
     const google_book_id = document.querySelector('input[name="bookid"]').value;
 
-    const title = document.querySelector('textarea[name="title"]').value;
-    const body = document.querySelector('textarea[name="review"]').value;
+    //const title = document.querySelector('textarea[name="title"]').value;
+    const textareaElement = document.getElementById('titlet');
+const title = textareaElement.value;
+const textareaElement1 = document.getElementById('reviewt');
+const body = textareaElement1.value;
+   // const body = document.querySelector('textarea[name="review"]').value;
     const scoreSelect = document.getElementById('score');
     const score = scoreSelect.value;
     const response = await fetch(`/savereview`, {
@@ -21,7 +25,7 @@ async function newReviewHandler(event) {
     });
 
     if (response.ok) {
-        document.location.replace('/review');
+        document.location.replace('/reviews');
     } else {
         alert(response.statusText);
     }
