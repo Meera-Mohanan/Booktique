d357b6e21a444fcd0c52b33eebe2204b2c167a65
Meera-Mohanan <126405858+Meera-Mohanan@users.noreply.github.com>
2023-07-11 21:04:12 +0800

    folders updated

--- public/js/login.js
+++ public/js/login.js
@@ -1,30 +1,31 @@
 async function loginFormHandler(event) {
-    event.preventDefault();
-  
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-  
-    if (email && password) {
-      const response = await fetch('/api/users/login', {
-        method: 'post',
-        body: JSON.stringify({
-          email,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  //console.log(response);
-      if (response.ok) {
-        const data=await response.json();
-        console.log(data);
-        document.location.replace('/'); 
-      } else {
-      
-        const data=await response.json();
-        document.getElementById("loginmessage").textContent =data.message; 
-        console.log("login failed");
-      }
+  event.preventDefault();
+
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (email && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'post',
+      body: JSON.stringify({
+        email,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+//console.log(response);
+    if (response.ok) {
+      const data=await response.json();
+      console.log(data);
+      document.location.replace('/'); 
+    } else {
+    
+      /* const data=await response.json();
+      //console.log(data);
+      document.getElementById("loginmessage").textContent =data.message; */
+      console.log("login failed");
     }
+  }
 }
 
 document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
